1da647d1f78b856a7b6f3d913b3bcc91d770b4c1
Nicolas <112518653+Nirclaw@users.noreply.github.com>
2023-06-04 14:58:08 -0500

    feat: :rocket: Teriminado el taller 9

---
+++ Taller9/main.php
@@ -0,0 +1,6 @@
+<?php
+
+$aliens=array('Zetaurianos','Xylophoids','Draconianos','Draconianos','Draconianos','Draconianos','Plasmoides','Quilaztlianos','Nebulianos','Gorgonitas','Centaurianos','Tritonianos','Saurinianos');
+
+print_r(array_unique($aliens));
+
